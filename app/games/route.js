0a34a0a85d712c128c17e77a073d9a082311bd04
David Cawton <dcawton@gmail.com>
2016-10-15 16:25:15 -0500

    Fetching Data

--- app/games/route.js
+++ app/games/route.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [
-      'Mario Kart',
-      'Donkey Kong',
-      'Mortal Kombat'
-    ];
+    return fetch('http://game-scores.herokuapp.com/games')
+    .then((response) => response.json())
+    .then((response) => response.data);
+
   }
 });
