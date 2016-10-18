a74e1b9a2d6a774f6ab72445efd0851197915f2d
David Cawton <dcawton@gmail.com>
2016-10-18 16:46:24 -0500

    Display Game Info

--- app/games/scores/route.js
+++ app/games/scores/route.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
-
+const { hash, all } = Ember.RSVP;
 export default Ember.Route.extend({
   model(params) {
     return fetch(`http://game-scores.herokuapp.com/games/${params.gameId}`)
       .then((res) => res.json())
-      .then((res) => res.data);
+      .then((res) => {
+        const scores = res.data.relationships.scores.data;
+        return hash({
+          game: res.data,
+          scores: all(scores.map((score) => {
+            return fetch(`http://game-scores.herokuapp.com/game-scores/${score.id}`)
+            .then((res) => res.json())
+            .then((res) => res.data);
+          })),
+        });
+      });
   }
 });
