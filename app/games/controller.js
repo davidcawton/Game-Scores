7713b31ac2faa25b11de038bd5829ba98ebea93b
David Cawton <dcawton@gmail.com>
2016-10-18 17:27:42 -0500

    Refactored Code for Ember Data

--- app/games/controller.js
+++ app/games/controller.js
@@ -2,10 +2,7 @@ import Ember from 'ember';
 const { set } = Ember;
 
 export default Ember.Controller.extend({
-  addNewGame(game) {
-    const currentGames = this.model;
-
-    set(this, 'model', [...currentGames, game]);
+  resetForm(game) {
     set(this, 'newGameName', '');
   }
 });
--- app/games/route.js
+++ app/games/route.js
@@ -2,36 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return fetch('http://game-scores.herokuapp.com/games')
-    .then((response) => response.json())
-    .then((response) => response.data);
-
+    return this.store.findAll('game');
   },
-    actions: {
-      saveNewGame(name,  ev) {
-        const body = {
-          data: {
-            type: 'games',
-            attributes: {
-              name
-            }
-          }
-        };
 
-        fetch('http://game-scores.herokuapp.com/games',  {
-          method: 'POST',
-          headers: {
-            Accept: 'application.json',
-            'Content-Type' : 'application.json',
-          },
-          body: JSON.stringify(body),
-        })
-          .then((response) => response.json())
-          .then((response) => {
-            this.controller.addNewGame(response.data);
-          });
+  actions: {
+    saveNewGame(name, ev) {
+      const game = this.store.createRecord('game', {
+        name
+      });
 
-        ev.preventDefault();
-      }
+      game.save()
+        .then(() => {
+          this.controller.resetForm(game);
+        });
+
+      ev.preventDefault();
     }
+  }
 });
